4c9e1ca9f5a16675849a83f4b76e0e9af6ae8d6d
Tango585 <lzklonely2016@gmail.com>
2024-08-21 11:01:10 +0800

    complete hw01

--- test.py
+++ test.py
@@ -1,2 +1,16 @@
-for i in range(10):
-    print(i)
+def hailstone(n):
+    k: int = n
+    i = 1
+    print(k)
+    while k != 1:
+        if k % 2 == 0:
+            k = k // 2
+            print(k)
+            i += 1
+        else:
+            k = 3 * k + 1
+            print(k)
+            i += 1
+    return i
+a=hailstone(27)
+print(a)
