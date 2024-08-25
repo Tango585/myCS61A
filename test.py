c54e6dd9e3e0073b124d500f9481cbcacd6031b2
Tango585 <lzklonely2016@gmail.com>
2024-08-25 23:12:52 +0800

    testproxy

--- test.py
+++ test.py
@@ -1,16 +1,1 @@
-def hailstone(n):
-    k: int = n
-    i = 1
-    print(k)
-    while k != 1:
-        if k % 2 == 0:
-            k = k // 2
-            print(k)
-            i += 1
-        else:
-            k = 3 * k + 1
-            print(k)
-            i += 1
-    return i
-a=hailstone(27)
-print(a)
+test0
